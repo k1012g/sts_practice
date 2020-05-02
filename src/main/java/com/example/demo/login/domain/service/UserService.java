2de5a71937fd7261643debbcfe2ac7fa707ce469
k1012g <kuri.ladybaby1012@gmail.com>
2020-05-02 16:19:18 +0900

    rowmapper function done

--- src/main/java/com/example/demo/login/domain/service/UserService.java
+++ src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.login.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.repository.UserDao;
@@ -10,6 +11,7 @@ import com.example.demo.login.domain.repository.UserDao;
 @Service
 public class UserService {
 	@Autowired
+	@Qualifier("UserDaoJdbcImpl4")
 	UserDao dao;
 	
 	public boolean insert(User user) {
